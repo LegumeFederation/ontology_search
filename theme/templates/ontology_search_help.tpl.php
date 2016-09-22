9ba178104a09ecea42aad09f5219ae5b241252a2
shivan4030 <shivan4030@gmail.com>
2016-09-22 15:53:44 -0500

    help page instructions updated

--- theme/templates/ontology_search_help.tpl.php
+++ theme/templates/ontology_search_help.tpl.php
@@ -1,8 +1,30 @@
-<h3>Module Description:</h3>
-<p>Place details about this module here.</p>
+<h2>Ontology Search Description:</h2>
+<p>
+This module helps user to search for a particular term and its relatives(Parents, Children, Siblings.) in an Ontology.
+</p>
 
-<h3>Setup Instructions:</h3>
-<p>Instructions for setup of this module go here.</p>
+<h2>Setup Instructions for Administrator:</h2>
+<p>
+<p> 1) The Administrator can restrict the Available Ontologies form which the user can search from .</p>
+<p> 2) Go to the Settings Tab.</p>
+<p> 3) Select multiple Available Ontologies which you want the user to be able to search from.</p>
+<p> 4) Save Changes.</p>
+</p>
 
-<h3>Features of this Module:</h3>
-<p>A description of this module's features go here.</p>
+<h2>User Instructions:</h2>
+<p>
+<p> 1) The user needs to enter the search term and select Ontology which he wants to search from the "Ontologies Available Tab" and click search.</p>
+<p> 2) If the user selects "Select All Available Ontologies"in the "Ontologies Available Tab" then user would get results from all the Ontologies Available.</p>
+<p> 3) Now user can select the specific term they are looking for along with the kind of relative(Child/Parent/Sibling) and click "Get Relative Terms" Tab.</p>
+<p> 4) User will get results containing "Degree(how far are they from the selected term), Name, Accession, Description, Ontology_Name" in a Table.</p>
+<p> 5) User Can now Traverse through the Ontology by clicking on any of these terms in the "Table" which will take them back to step-3.</p>    
+</p>
+
+<h2>Features of this Module:</h2>
+<p>
+<p>1) This Module enables user to search for a term in a specific Ontology.</p>
+<p>2) This Module enables user to search for a term in all the Ontologies.</p>
+<p>3) This Module enables user to find Parent, Children, Siblings for a term in the ontologies selected.</p>
+<p>4) This Module enables the user to recursively traverse the Ontology.</p>
+<p>5) This Module gives back the information for the search terms like "Degree (how far are they from the selected term), Name, Accession, Description, Ontology_Name".<p>
+</p>
